8e964ce59ded2d7d4a6afe2310217fc9d6c0e42a
lema <lemajunior@hotmail.com>
2022-03-10 08:19:54 +0300

    changed ip for testing online

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -1,9 +1,9 @@
 export default {
   state: {
-    // apiURL: 'https://naturalhistorysafari.com/core/api/v1',
-    apiURL: 'http://localhost:8000/api/v1',
-    // baseURL: 'https://dashboard.naturalhistorysafari.com'
-    baseURL: 'http://localhost:8000'
+    apiURL: 'https://naturalhistorysafari.com/core/api/v1',
+    // apiURL: 'http://localhost:8000/api/v1',
+    baseURL: 'https://dashboard.naturalhistorysafari.com'
+    // baseURL: 'http://localhost:8000'
   },
   getters: {
     apiURL: state => { return state.apiURL },
